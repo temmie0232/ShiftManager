00683d1589f7cae6a06d0ee9e7d46015549487fe
temmie0232 <temmie0232@gmail.com>
2024-11-29 23:08:43 +0900

    add: PostgreSQLの設定

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shift_management',
+        'USER': 'shift_admin',
+        'PASSWORD': 'D3tvn426',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
