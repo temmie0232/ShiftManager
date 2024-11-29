b96948aa5e96e12449b26e457a69dc6d03711123
temmie0232 <temmie0232@gmail.com>
2024-11-29 23:24:09 +0900

    add: シフトモデルの作成

--- backend/shifts/models.py
+++ backend/shifts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ShiftRequest(models.Model):
+    employee = models.ForeignKey('accounts.Employee', on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class MonthlyPreference(models.Model):
+    employee = models.ForeignKey('accounts.Employee', on_delete=models.CASCADE)
+    year_month = models.DateField()
+    min_hours = models.IntegerField()
+    max_hours = models.IntegerField()
+    min_days_per_week = models.IntegerField()
+    max_days_per_week = models.IntegerField()
+
+class ConfirmedShift(models.Model):
+    employee = models.ForeignKey('accounts.Employee', on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
