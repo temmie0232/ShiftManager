d9309a084f4d41ed327040ef69e8fa269b399353
temmie0232 <temmie0232@gmail.com>
2024-11-30 13:57:48 +0900

    feat: / にアクセスしたら /home にリダイレクト

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="min-h-screen grid place-items-center">
-      <h1>Hello World</h1>
-    </div>
-  );
+  redirect("/home");
 }
