c644990fff56456c4d2f3d1102bc465c72c903b3
temmie0232 <temmie0232@gmail.com>
2024-12-05 11:47:58 +0900

    update: フッターにリポジトリの追加

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Github } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "シフト管理",
-  description: "シフト管理アプリ",
 };
 
 export default function RootLayout({
@@ -20,6 +20,14 @@ export default function RootLayout({
         </main>
         <footer className="py-2 px-8 bg-[#F5F6F8]">
           <div className="max-w-4xl mx-auto flex justify-center items-center text-sm text-gray-500 relative">
+            <a
+              href="https://github.com/temmie0232/ShiftManager"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="absolute left-0 hover:text-gray-700 transition-colors"
+            >
+              <Github size={20} />
+            </a>
             <div>© 2024 Yuta Yaginuma</div>
             <a
               href="mailto:temmie0232@gmail.com?subject=シフト管理アプリの問題報告"
